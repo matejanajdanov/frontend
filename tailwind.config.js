52314653474727cbf8d4fcce606f011080826512
matejanajdanov <matejanajdanov0@gmail.com>
2022-12-08 23:01:20 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#0B82B8",
+        main500: "#0b4470",
+      },
+    },
+  },
+  plugins: [],
+};
